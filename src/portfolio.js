fb0a4fc661152c5b37576a7775e7352f9b5efa87
Sanket-09 <f20200356@hyderabad.bits-pilani.ac.in>
2023-09-07 23:57:47 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,605 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Sanket",
+  logo_name: "<sj()>",
+  full_name: "Sanket Jaiswal",
+  subTitle:
+    "Full Stack Developer, Flutter & Badminton Enthusiast, Life Long Learner.",
+  resumeLink:
+    "https://drive.google.com/file/d/1Z7dC6YBJyM2hx_EjFYz36q3QNPxhGmWY/view?usp=sharing",
+  mail: "mailto:im.sanketjaiswal.09@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/Sanket-09",
+  linkedin: "https://www.linkedin.com/in/sanket-jaiswal-6431971ba/",
+  gmail: "im.sanketjaiswal.09@gmail.com",
+  facebook: "https://www.facebook.com/sanket.jaiswal.100/",
+  twitter: "https://twitter.com/SanketJ25977484",
+  instagram: "https://www.instagram.com/sanket._jaiswal/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive User Interfaces and Backend for web applications",
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Creating application backend in Node, Express, Springboot and ASP.net",
+        "⚡ Integration of third party services such as Firebase",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "Bootstrap",
+          fontAwesomeClassname: "simple-icons:bootstrap",
+          style: {
+            color: "#563d7c",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:nodedotjs",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: ".NET",
+          fontAwesomeClassname: "simple-icons:dotnet",
+          style: {
+            color: "#512BD4",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+
+        {
+          skillName: "Springboot",
+          fontAwesomeClassname: "simple-icons:springboot",
+          style: {
+            color: "#6DB33F",
+          },
+        },
+
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C#",
+          fontAwesomeClassname: "simple-icons:csharp",
+          style: {
+            color: "#239120",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "BITS Pilani",
+      subtitle: "Major: Bachelors in Electronics & Communication",
+      logo_path: "BITS_logo.png",
+      alt_name: "SSEC",
+      duration: "2020 - 2024",
+      descriptions: [
+        "⚡ I'm currently pursuing my Bachelors in Electronics and Communication Engineering.",
+        "⚡ I have studied core subjects like Communication Networks, Information Theory and Coding, Communication System, etc.",
+        "⚡ I have also explored fields by studying subjects such as Principle of Economics, Principle of Management, Discrete Maths, Human Resource Developement, New Venture Creation, etc.",
+        // "⚡ I have implemented several projects based on what I've learned under my Computer Engineering course. ",
+      ],
+    },
+    {
+      title: "Minor Degree Institution",
+      subtitle: "Minor: Computing and Intelligence",
+      logo_path: "BITS_logo.png",
+      alt_name: "SSEC",
+      duration: "2020 - 2024",
+      descriptions: [
+        "⚡ I have also pursued a minor degree in Computing and Intelligence alongside my major.",
+        "⚡ I have studied core subjects like Data Structures, DBMS, OOPS, OS, Blockchain, AI, etc.",
+      ],
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Flutter",
+      subtitle: "Flutter Widgets",
+      logo_path: "linkedin_learning.png",
+      certificate_link:
+        "https://www.linkedin.com/learning/certificates/18fad6665f961562f7bc6a75ebe24e9077c6ad57932b20652a0712ac39b57b07",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "white",
+    },
+
+    {
+      title: "Flutter",
+      subtitle: "Organizing Flutter Code",
+      logo_path: "linkedin_learning.png",
+      certificate_link:
+        "https://www.linkedin.com/learning/certificates/a56f850525077ad6d0aa14a4b7b6c8148469815957c06a28cbe685cb1c65a74d",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "white",
+    },
+
+    {
+      title: "Firebase",
+      subtitle: "Firebase for Flutter",
+      logo_path: "linkedin_learning.png",
+      certificate_link:
+        "https://www.linkedin.com/learning/certificates/ec540803895e95a98f723396236b59b30012ddea684c113a4d9fdcadaa90923d?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BHMFG5qTsTZ%2BIC9S%2BdDxQvA%3D%3D",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "white",
+    },
+
+    {
+      title: "C++",
+      subtitle: "Master C++",
+      logo_path: "linkedin_learning.png",
+      certificate_link:
+        "https://www.linkedin.com/learning/certificates/612d2500f64ab2b1d1024c564ba88d097b0ca17392bae1f5fb2af6e737344e6d",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "white",
+    },
+
+    {
+      title: "SQL",
+      subtitle: "Master SQL",
+      logo_path: "linkedin_learning.png",
+      certificate_link:
+        "https://www.linkedin.com/learning/certificates/69e8dfa1e2518ac30aa291d7f374021082f00c0d1824167d45e4a5d30997f7d7?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BhD%2BVNq0pQVqfgHZbYecy7w%3D%3D",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "white",
+    },
+
+    {
+      title: "Flutter",
+      subtitle: "Flutter with dart",
+      logo_path: "london_app.jpeg",
+      certificate_link:
+        "https://www.linkedin.com/learning/certificates/69e8dfa1e2518ac30aa291d7f374021082f00c0d1824167d45e4a5d30997f7d7?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BhD%2BVNq0pQVqfgHZbYecy7w%3D%3D",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "white",
+    },
+
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've completed one internship, and I also author new solutions on Chegg where i work part-time as Subject Matter Expert. I've mostly done projects on my own as part of college curriculam and few based on my interest as well. I am actively looking for internships.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "Software Developement Engineer Intern",
+          company: "IDS Infotech",
+          company_url: "https://idsil.com/",
+          logo_path: "ids_infotech.png",
+          duration: "May 2022 - July 2022",
+          location: "Remote",
+          description: `I contributed to project which included .net core framework, C#, MVC, MySQL, React etc. The project consisted of implementing a charity management system that bridges the gap between and NGO and a Donor. I worked towards streamlining the user experience of the web app by creating separate views for teh end user.
+          `,
+          color: "#0071C5",
+        },
+        {
+          title: "Managed Network Expert",
+          company: "Chegg India",
+          company_url: "hhttps://thesparksfoundation.info/",
+          logo_path: "chegg.png",
+          duration: "Feb 2022 - Present",
+          location: "Remote",
+          description: `Performed quality assurance for the average solutions alongwith authoring ( New solution creation ) for Textbook questions.
+          `,
+          // "Created Front end of Yearn Financial Mutual Funds website. also degined simple web application for better user experience, designed DB Schemas as well.",
+          color: "#ee3c26",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Operations Coordinator",
+          company: "Placement Unit",
+          company_url:
+            "https://universe.bits-pilani.ac.in/hyderabad/admissions/PlacementUnit",
+          logo_path: "PU.png",
+          duration: "Mar 2020 - Nov 2020",
+          location: "BITS Pilani",
+          description:
+            "Responsible for end-to-end placement process involving Pitching, Scheduling & Hosting companies for Full-time engineering of 1200 students as well as Summer Internship of 1034 students.",
+          color: "#4285F4",
+        },
+        {
+          title: "Tech Team Member",
+          company: "Phoenix Association",
+          company_url: "https://www.facebook.com/phoenix.bphc/",
+          logo_path: "phoenix.jpeg",
+          duration: "September 2021 - Present",
+          location: "BITS Pilani",
+          description:
+            "Held numerous workshops and distributed learning content for freshers. Conducted Robowars & Line follower robot race during the annual Technical fest - ATMOS.",
+          color: "#a60000",
+        },
+
+        {
+          title: "German Division Member",
+          company: "Sanskrit & Foreign Language Club",
+          company_url: "https://www.facebook.com/saflbphc/",
+          logo_path: "safl.jpg",
+          duration: "April 2021 - September 2022",
+          location: "BITS Pilani",
+          description:
+            "Conducted various competetions including Bamboozled and Around the world during the annual cultural fest - PEARL.",
+          color: "#ffffff",
+        },
+        {
+          title: "Student Volunteer",
+          company: "National Service Scheme",
+          company_url: "http://nssbphc.in/",
+          logo_path: "NSS.jfif",
+          duration: "April 2021 - Present ",
+          location: "BITS Pilani",
+          description:
+            "Built NSS mobile application based on flutter to monitor and keep track of the club activities.",
+          color: "#282b6c",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, Flutter Mobile Applications, and React Project. Below are some of my projects..",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "profile.png",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Facebook Helpdesk",
+      url: "https://github.com/Sanket-09/Richpanel",
+      description:
+        "This app allows clients to connect their facebook accounts, listen to their messenger messages, and reply to them within the application.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "simple-icons:mongodb",
+        },
+
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Node.js",
+          iconifyClass: "simple-icons:nodedotjs",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "Charity Management Application",
+      url: "https://github.com/Sanket-09/IDSps1",
+      description:
+        "The project consisted of implementing a Charity Management System that bridges the gap between an NGO and a Donor.",
+      languages: [
+        {
+          name: "ASP.net",
+          iconifyClass: "logos-dotnet",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos-bootstrap",
+        },
+        {
+          name: "C#",
+          iconifyClass: "simple-icons:csharp",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Restaurants Review System",
+      url: "https://github.com/Sanket-09/restaurants",
+      description:
+        "The app includes a comprehensive list of restaurants with advanced filtering options. It also enables geolocation integrations for precise restaurant locations.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "MongoDB",
+          iconifyClass: "simple-icons:mongodb",
+        },
+
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Node.js",
+          iconifyClass: "simple-icons:nodedotjs",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "Grocery Management System",
+      url: "https://github.com/Sanket-09/restaurants",
+      description:
+        "A fully functional E-commerce website with admin, manager & cust. role.Implemented hierarchial access for admin,manager and user using access modifiers",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos-bootstrap",
+        },
+        {
+          name: "Springboot",
+          iconifyClass: "simple-icons:springboot",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "BMI Calculator",
+      url: "https://github.com/Sanket-09/BMI_Calci",
+      description:
+        "The Body Mass Index (BMI) Calculator can be used to calculate BMI value and corresponding weight status while taking age into consideration. .",
+      languages: [
+        {
+          name: "Flutter",
+          iconifyClass: "logos-flutter",
+        },
+        {
+          name: "Dart",
+          iconifyClass: "logos-dart",
+        },
+      ],
+    },
+    {
+      id: "5",
+      name: "World Clock",
+      url: "https://github.com/Sanket-09/world_clock",
+      description:
+        "A simple world clock application which displays time, weather, and the background chnges according to the time and weather",
+      languages: [
+        {
+          name: "Flutter",
+          iconifyClass: "logos-flutter",
+        },
+        {
+          name: "Dart",
+          iconifyClass: "logos-dart",
+        },
+      ],
+    },
+    {
+      id: "6",
+      name: "Login Authenticator",
+      url: "https://github.com/Sanket-09/LastPage",
+      description:
+        "Made a mobile app which uses firebase google and email authentication to verify the user",
+      languages: [
+        {
+          name: "Flutter",
+          iconifyClass: "logos-flutter",
+        },
+        {
+          name: "Dart",
+          iconifyClass: "logos-dart",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+
+    {
+      id: "7",
+      name: "Portfolio Website",
+      url: "https://github.com/Sanket-09/Portfolio-website",
+      description: "Current Portfolio project",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
